b18a9a862d2396871861217c0cad8a727372af3d
Volodymyr Kashnikov <vkashnikov19@gmail.com>
2025-03-18 22:53:05 +0200

    Add ctrlWrapper to recipes

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,66 +1,61 @@
 import Recipes from "../models/Recipes.js";
 import Ingredients from "../models/Ingredients.js";
+import ctrlWrapper from "../helpers/ctrlWrapper.js";
+import HttpError from "../helpers/HttpError.js";
 
 
-export async function getRecipeById(req, res, next) {
-  try {
-    const { id } = req.params;
+const getRecipeById = async (req, res) => {
+  const { id } = req.params;
 
-    const recipe = await Recipes.findByPk(id);
-
-    if (!recipe) {
-      return res.status(404).json({ message: "Recipe not found" });
-    }
-
-    const ingredientIds = recipe.ingredientsList.map((ing) => ing.ingredientId);
-
-    const ingredients = await Ingredients.findAll({
-      where: { id: ingredientIds },
-    });
-
-    const ingredientsWithMeasure = ingredients.map((ing) => {
-      const found = recipe.ingredientsList.find((i) => i.ingredientId === ing.id);
-      return {
-        id: ing.id,
-        name: ing.name,
-        measure: found ? found.measure : "N/A",
-        decs: ing.decs,
-        img: ing.img,
-      };
-    });
-
-    res.json({
-      status: "success",
-      data: {
-        title: recipe.title,
-        category: recipe.category,
-        owner: recipe.owner,
-        area: recipe.area,
-        instructions: recipe.instructions,
-        description: recipe.description,
-        thumb: recipe.thumb,
-        time: recipe.time,
-        ingredients: ingredientsWithMeasure,
-      },
-    });
-  } catch (error) {
-    next(error);
+  const recipe = await Recipes.findByPk(id);
+  if (!recipe) {
+    throw HttpError(404, "Recipe not found");
   }
-}
 
-export async function getMyRecipes(req, res, next) {
-  try {
-    const userId = req.user.id; 
+  const ingredientIds = recipe.ingredientsList.map((ing) => ing.ingredientId);
+  const ingredients = await Ingredients.findAll({ where: { id: ingredientIds } });
 
-    const myRecipes = await Recipes.findAll({
-      where: { owner: userId },
-    });
+  const ingredientsWithMeasure = ingredients.map((ing) => {
+    const found = recipe.ingredientsList.find((i) => i.ingredientId === ing.id);
+    return {
+      id: ing.id,
+      name: ing.name,
+      measure: found ? found.measure : "N/A",
+      decs: ing.decs,
+      img: ing.img,
+    };
+  });
 
-    res.json({
-      status: "success",
-      data: myRecipes,
-    });
-  } catch (error) {
-    next(error);
-  }
-}
+  res.json({
+    status: "success",
+    data: {
+      title: recipe.title,
+      category: recipe.category,
+      owner: recipe.owner,
+      area: recipe.area,
+      instructions: recipe.instructions,
+      description: recipe.description,
+      thumb: recipe.thumb,
+      time: recipe.time,
+      ingredients: ingredientsWithMeasure,
+    },
+  });
+};
+
+const getMyRecipes = async (req, res) => {
+  const userId = req.user.id;
+
+  const myRecipes = await Recipes.findAll({
+    where: { owner: userId },
+  });
+
+  res.json({
+    status: "success",
+    data: myRecipes,
+  });
+};
+
+export default {
+  getRecipeById: ctrlWrapper(getRecipeById),
+  getMyRecipes: ctrlWrapper(getMyRecipes),
+};
