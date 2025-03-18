e09909359fe8007cb7307cd1bd3f304fd6f66453
Volodymyr Kashnikov <vkashnikov19@gmail.com>
2025-03-18 22:32:21 +0200

    add ingridients list

--- db/models/Recipes.js
+++ db/models/Recipes.js
@@ -2,40 +2,42 @@ import sequelize from "../sequelize.js";
 import { DataTypes } from "sequelize";
 
 const Recipes = sequelize.define("recipe", {
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    category: {
-        type: DataTypes.STRING,
-        allowNull: false,   
-    },
-    owner: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    area: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    instructions: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    description: {
-        type: DataTypes.TEXT,
-        allowNull: false     
-    },
-    thumb: {
-        type: DataTypes.STRING,
-        allowNull: false 
-    },
-    time: {
-        type: DataTypes.INTEGER,
-        allowNull: false 
-    },
-})
-
-// Recipes.sync();
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  owner: {
+    type: DataTypes.STRING, 
+    allowNull: false,
+  },
+  area: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  instructions: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  thumb: {
+    type: DataTypes.STRING, 
+    allowNull: false,
+  },
+  time: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  ingredientsList: {
+    type: DataTypes.JSON,
+    allowNull: false,
+  },
+});
 
 export default Recipes;
