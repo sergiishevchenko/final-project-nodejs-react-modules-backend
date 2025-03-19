d2868aa6d9793a1564413febb65829567089e0c6
Anastasiia Dushka <143402218+Arnary@users.noreply.github.com>
2025-03-19 14:51:48 +0200

    Update ingredientsServices.js
    increase pagination limit

--- services/ingredientsServices.js
+++ services/ingredientsServices.js
@@ -1,7 +1,7 @@
 import Ingredients from '../db/models/Ingredients.js';
 
 const getIngredients = (query = {},  pagination = {}) => {
-  const { page = 1, limit = 10 } = pagination;
+  const { page = 1, limit = 600 } = pagination;
   const normalizedLimit = Number(limit);
   const offset = (Number(page) - 1) * normalizedLimit;
   return Ingredients.findAll({ where: query, offset, limit: normalizedLimit });
