efec5c4728ffe7f7b36e7287cc758b5770910d64
Anastasiia Dushka <143402218+Arnary@users.noreply.github.com>
2025-03-19 12:39:11 +0200

    Update categoriesControllers.js
    remove pagination

--- controllers/categoriesControllers.js
+++ controllers/categoriesControllers.js
@@ -2,8 +2,7 @@ import ctrlWrapper from '../helpers/ctrlWrapper.js';
 import getCategories from '../services/categoriesServices.js';
 
 const getAllCategories = async (req, res) => {
-    const { page = 1, limit = 10 } = req.query;
-    const result = await getCategories({ page, limit });
+    const result = await getCategories();
     res.json(result);
 };
 
