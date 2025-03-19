4ef286e49f063f42472c3442e8e73674b68b3ed0
Volodymyr Kashnikov <vkashnikov19@gmail.com>
2025-03-19 21:08:34 +0200

    Merge branch 'dev' into feature/get-recipe-detail

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -4,54 +4,156 @@ import Ingredients from "../db/models/Ingredients.js";
 import Areas from "../db/models/Areas.js";
 import RecipesIngredients from "../db/models/RecipesIngredients.js";
 import getPagination from "../helpers/getPagination.js";
+import HttpError from "../helpers/HttpError.js";
 
+/**
+ * Отримання рецепта за ID (публічний)
+ */
+export const getRecipeById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
 
-export const searchRecipes = async (req, res) => {
-    const { category, ingredient, area, page, limit: size } = req.query;
-    const { limit, offset } = getPagination(page, size);
-
-    const whereClause = {};
-    const include = [];
-
-    if (category) {
-        const categoryRecord = await Categories.findOne({ where: { name: category } });
-        if (categoryRecord) {
-            whereClause.categoryId = categoryRecord.id;
-        }
-    }
-
-    if (area) {
-        const areaRecord = await Areas.findOne({ where: { name: area } });
-        if (areaRecord) {
-            whereClause.areaId = areaRecord.id;
-        }
-    }
-
-    if (ingredient) {
-        const ingredientRecord = await Ingredients.findOne({ where: { name: ingredient } });
-        if (ingredientRecord) {
-            include.push({
-                model: Ingredients,
-                through: {
-                    model: RecipesIngredients,
-                    where: { ingredientId: ingredientRecord.id },
+        const recipe = await Recipes.findByPk(id, {
+            include: [
+                {
+                    model: Categories,
+                    as: "category",
+                    attributes: ["name"],
                 },
-                as: 'ingredients'
-            });
+                {
+                    model: Areas,
+                    as: "area",
+                    attributes: ["name"],
+                },
+                {
+                    model: Ingredients,
+                    through: {
+                        model: RecipesIngredients,
+                        attributes: ["measure"],
+                    },
+                    as: "ingredients",
+                },
+            ],
+        });
+
+        if (!recipe) {
+            throw HttpError(404, "Recipe not found");
         }
+
+        res.json({
+            status: "success",
+            data: {
+                id: recipe.id,
+                title: recipe.title,
+                category: recipe.category?.name,
+                area: recipe.area?.name,
+                description: recipe.description,
+                instructions: recipe.instructions,
+                thumb: recipe.thumb,
+                time: recipe.time,
+                ingredients: recipe.ingredients.map((ing) => ({
+                    id: ing.id,
+                    name: ing.name,
+                    measure: ing.recipe_ingredient.measure,
+                    img: ing.img,
+                    desc: ing.desc,
+                })),
+            },
+        });
+    } catch (error) {
+        next(error);
     }
+};
 
-    const { count, rows } = await Recipes.findAndCountAll({
-        where: whereClause,
-        include: include,
-        limit,
-        offset,
-    });
+/**
+ * Отримання власних рецептів користувача (приватний)
+ */
+export const getMyRecipes = async (req, res, next) => {
+    try {
+        const userId = req.user.id;
 
-    res.json({
-        totalItems: count,
-        recipes: rows,
-        totalPages: Math.ceil(count / limit),
-        currentPage: page ? +page : 0,
-    });
+        const myRecipes = await Recipes.findAll({
+            where: { ownerId: userId },
+        });
+
+        if (!myRecipes.length) {
+            throw HttpError(404, "You have no recipes");
+        }
+
+        res.json({
+            status: "success",
+            data: myRecipes,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+/**
+ * Пошук рецептів за фільтрами (категорія, інгредієнт, регіон)
+ */
+export const searchRecipes = async (req, res, next) => {
+    try {
+        const { category, ingredient, area, page, limit: size } = req.query;
+        const { limit, offset } = getPagination(page, size);
+
+        const whereClause = {};
+        const include = [];
+
+        console.log(`Searching recipes with: category=${category}, ingredient=${ingredient}, area=${area}`);
+
+        if (category) {
+            const categoryRecord = await Categories.findOne({ where: { name: category } });
+            if (categoryRecord) {
+                whereClause.categoryId = categoryRecord.id;
+            } else {
+                throw HttpError(404, "Category not found");
+            }
+        }
+
+        if (area) {
+            const areaRecord = await Areas.findOne({ where: { name: area } });
+            if (areaRecord) {
+                whereClause.areaId = areaRecord.id;
+            } else {
+                throw HttpError(404, "Area not found");
+            }
+        }
+
+        if (ingredient) {
+            const ingredientRecord = await Ingredients.findOne({ where: { name: ingredient } });
+            if (ingredientRecord) {
+                include.push({
+                    model: Ingredients,
+                    through: {
+                        model: RecipesIngredients,
+                        where: { ingredientId: ingredientRecord.id },
+                    },
+                    as: "ingredients",
+                });
+            } else {
+                throw HttpError(404, "Ingredient not found");
+            }
+        }
+
+        const { count, rows } = await Recipes.findAndCountAll({
+            where: whereClause,
+            include: include.length > 0 ? include : undefined,
+            limit,
+            offset,
+        });
+
+        if (!rows.length) {
+            throw HttpError(404, "No recipes found with given criteria");
+        }
+
+        res.json({
+            totalItems: count,
+            recipes: rows,
+            totalPages: limit > 0 ? Math.ceil(count / limit) : 1,
+            currentPage: page ? +page : 1,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
--- db/models/Recipes.js
+++ db/models/Recipes.js
@@ -2,45 +2,6 @@ import sequelize from "../sequelize.js";
 import { DataTypes } from "sequelize";
 
 const Recipes = sequelize.define("recipe", {
-<<<<<<< HEAD
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  categoryId: {
-    type: DataTypes.INTEGER, 
-    allowNull: false,
-  },
-  ownerId: {
-    type: DataTypes.INTEGER, 
-    allowNull: false,
-  },
-  areaId: {
-    type: DataTypes.INTEGER, 
-    allowNull: false,
-  },
-  instructions: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  thumb: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  time: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  ingredientsList: {
-    type: DataTypes.JSON, 
-    allowNull: false,
-  },
-});
-=======
     title: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -74,7 +35,6 @@ const Recipes = sequelize.define("recipe", {
         allowNull: false
     },
 })
->>>>>>> dev
 
 // Recipes.sync()
 
