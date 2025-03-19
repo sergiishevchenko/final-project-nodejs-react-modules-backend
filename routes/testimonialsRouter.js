b3605dd7356da4294e4242e31168c0b3d4a900a4
Anastasiia Dushka <143402218+Arnary@users.noreply.github.com>
2025-03-19 14:33:47 +0200

    Update testimonialsRouter.js
    add ctrlWrapper

--- routes/testimonialsRouter.js
+++ routes/testimonialsRouter.js
@@ -3,9 +3,10 @@ import express from "express";
 import { getTestimonials } from "../controllers/testimonialsControllers.js";
 import { getTestimonialsSchema } from "../schemas/testimonialsSchemas.js";
 import validateQuery from "../helpers/validateQuery.js";
+import ctrlWrapper from "../helpers/ctrlWrapper.js";
 
 const testimonialsRouter = express.Router();
 
-testimonialsRouter.get("/", validateQuery(getTestimonialsSchema), getTestimonials);
+testimonialsRouter.get("/", validateQuery(getTestimonialsSchema), ctrlWrapper(getTestimonials));
 
 export default testimonialsRouter;
