24982fc91642584d4e399359d9029be95c085b36
Anastasiia Dushka <143402218+Arnary@users.noreply.github.com>
2025-03-19 14:51:31 +0200

    Update ingredientsControllers.js
    increase pagination limit

--- controllers/ingredientsControllers.js
+++ controllers/ingredientsControllers.js
@@ -2,7 +2,7 @@ import ctrlWrapper from "../helpers/ctrlWrapper.js";
 import getIngredients from "../services/ingredientsServices.js";
 
 const getAllIngredients = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 600 } = req.query;
   const result = await getIngredients({}, { page, limit });
   res.json(result);
 };
