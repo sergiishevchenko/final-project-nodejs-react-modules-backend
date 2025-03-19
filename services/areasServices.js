238404efa2dd8d3f84c0452238a58a9274d64c7d
Anastasiia Dushka <143402218+Arnary@users.noreply.github.com>
2025-03-19 12:27:14 +0200

    Update areasServices.js
    remove pagination

--- services/areasServices.js
+++ services/areasServices.js
@@ -1,10 +1,7 @@
 import Areas from "../db/models/Areas.js";
 
-const getAreas = (query = {}, pagination = {}) => {
-  const { page = 1, limit = 10 } = pagination;
-  const normalizedLimit = Number(limit);
-  const offset = (Number(page) - 1) * normalizedLimit;
-  return Areas.findAll({ where: query, offset, limit: normalizedLimit });
+const getAreas = () => {
+  return Areas.findAll();
 };
 
 export default getAreas;
