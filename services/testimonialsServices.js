c361e76fb4b7cfbe0715b4990860f33a5823e29d
Anastasiia Dushka <143402218+Arnary@users.noreply.github.com>
2025-03-19 14:31:12 +0200

    Update testimonialsServices.js
    removed the part of the code that caused the error "user is not associated to testimonial!"

--- services/testimonialsServices.js
+++ services/testimonialsServices.js
@@ -4,13 +4,6 @@ import Users from "../db/models/Users.js";
 async function getAllTestimonials(page, limit) {
 
     const rows = await Testimonials.findAll({
-        include: [
-            {
-                model: Users,
-                as: 'user',
-                attributes: ['id', 'name', "avatar"]
-            }
-        ],
         offset: (page - 1) * limit,
         limit: limit,
         order: [['id', 'DESC']],
