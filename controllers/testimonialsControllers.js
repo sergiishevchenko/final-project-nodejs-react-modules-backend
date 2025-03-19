ec9d69c42f9ef937f2c7c17d2ebca015617d78a5
Anastasiia Dushka <143402218+Arnary@users.noreply.github.com>
2025-03-19 14:34:30 +0200

    Update testimonialsControllers.js

--- controllers/testimonialsControllers.js
+++ controllers/testimonialsControllers.js
@@ -1,11 +1,7 @@
 import getAllTestimonials from "../services/testimonialsServices.js";
 
 export const getTestimonials = async (req, res, next) => {
-    try {
         const { page = 1, limit = 3 } = req.query;
         const { rows, count } = await getAllTestimonials(page, limit);
         res.json({ total: count, testimonials: rows });
-    } catch (error) {
-        next(error);
-    }
 };
