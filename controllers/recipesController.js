d39dfd6d55fbbc4b441d18665b41651223c25953
Anastasiia Dushka <143402218+Arnary@users.noreply.github.com>
2025-03-20 13:20:17 +0200

    Update recipesController.js
    fix conflict bugs

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -4,77 +4,9 @@ import Ingredients from "../db/models/Ingredients.js";
 import Areas from "../db/models/Areas.js";
 import RecipesIngredients from "../db/models/RecipesIngredients.js";
 import getPagination from "../helpers/getPagination.js";
-
 import HttpError from "../helpers/HttpError.js";
 import { createRecipe, getPopularRecipes, deleteRecipe } from "../services/recipesServices.js";
-
 import { Op } from "sequelize";
-
-
-
-/**
- * Пошук рецептів за фільтрами (категорія, інгредієнт, регіон)
- */
-export const searchRecipes = async (req, res) => {
-    const { category, ingredient, area, page, limit:size } = req.query;
-    const { limit, offset } = getPagination(page, size);
-
-    const whereClause = {};
-    const include = [];
-
-        if (category) {
-            const categoryRecord = await Categories.findOne({ where: { name: category } });
-            if (categoryRecord) {
-                whereClause.categoryId = categoryRecord.id;
-            } else {
-                throw HttpError(404, "Category not found");
-            }
-
-        if (area) {
-            const areaRecord = await Areas.findOne({ where: { name: area } });
-            if (areaRecord) {
-                whereClause.areaId = areaRecord.id;
-            } else {
-                throw HttpError(404, "Area not found");
-            }
-
-    if (ingredient) {
-        const ingredientRecord = await Ingredients.findOne({ where: { name: ingredient } });
-        if (ingredientRecord) {
-            const recipeIds = await RecipesIngredients.findAll({
-                where: { ingredientId: ingredientRecord.id },
-                attributes: ['recipeId']
-            });
-          
-            if (recipeIds.length > 0) {
-                whereClause.id = { [Op.in]: recipeIds.map(item => item.recipeId) };
-            } else {
-                return res.json({
-                    totalItems: 0,
-                    recipes: [],
-                    totalPages: 0,
-                    currentPage: page ? +page : 0,
-                });
-         } else {
-              throw HttpError(404, "Ingredient not found");
-         }
-        }
-    }
-        
-        const { count, rows } = await Recipes.findAndCountAll({
-            where: whereClause,
-            include: include.length > 0 ? include : undefined,
-            limit,
-            offset,
-        });
-
-        res.json({
-            totalItems: count,
-            recipes: rows,
-            totalPages: limit > 0 ? Math.ceil(count / limit) : 1,
-            currentPage: page ? +page : 1,
-        });
-};
 
 /**
  * Отримання популярних рецептів
@@ -197,4 +129,69 @@ export const getMyRecipes = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-};          
+};
+
+/**
+ * Пошук рецептів за фільтрами (категорія, інгредієнт, регіон)
+ */
+export const searchRecipes = async (req, res) => {
+    const { category, ingredient, area, page, limit:size } = req.query;
+    const { limit, offset } = getPagination(page, size);
+
+    const whereClause = {};
+    const include = [];
+
+    if (category) {
+        const categoryRecord = await Categories.findOne({ where: { name: category } });
+        if (categoryRecord) {
+            whereClause.categoryId = categoryRecord.id;
+        }
+    } else {
+        throw HttpError(404, "Category not found");
+    }
+    if (area) {
+        const areaRecord = await Areas.findOne({ where: { name: area } });
+        if (areaRecord) {
+            whereClause.areaId = areaRecord.id;
+        }
+    } else {
+        throw HttpError(404, "Area not found");
+    }
+
+    if (ingredient) {
+        const ingredientRecord = await Ingredients.findOne({ where: { name: ingredient } });
+        if (ingredientRecord) {
+            const recipeIds = await RecipesIngredients.findAll({
+                where: { ingredientId: ingredientRecord.id },
+                attributes: ['recipeId']
+            });
+          
+            if (recipeIds.length > 0) {
+                whereClause.id = { [Op.in]: recipeIds.map(item => item.recipeId) };
+            } else {
+                return res.json({
+                    totalItems: 0,
+                    recipes: [],
+                    totalPages: 0,
+                    currentPage: page ? +page : 0,
+                });
+            }
+        }
+    } else {
+        throw HttpError(404, "Ingredient not found");
+    }
+        
+        const { count, rows } = await Recipes.findAndCountAll({
+            where: whereClause,
+            include: include.length > 0 ? include : undefined,
+            limit,
+            offset,
+        });
+
+        res.json({
+            totalItems: count,
+            recipes: rows,
+            totalPages: limit > 0 ? Math.ceil(count / limit) : 1,
+            currentPage: page ? +page : 1,
+        });
+};
