b05ab860df8af139df378b8d29a3d4af08907818
Volodymyr Kashnikov <vkashnikov19@gmail.com>
2025-03-20 21:36:12 +0200

    Add pagination to user's recipes retrieval

--- helpers/getPagination.js
+++ helpers/getPagination.js
@@ -1,8 +1,6 @@
 const getPagination = (page, size) => {
   const limit = size ? +size : 9;
-  const offset = page ? page * limit : 0;
-
+  const offset = page > 1 ? (page - 1) * limit : 0; // ✅ Виправлений offset
   return { limit, offset };
 };
-
 export default getPagination;
