0e669263c4727e36145be7d818df2289eb586e78
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-11-17 09:43:28 +0100

    Refactor database configuration in sequelize.js to improve SSL handling and set default values for environment variables

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,16 +1,30 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
-    dialect: process.env.DATABASE_DIALECT,
-    username: process.env.DATABASE_USERNAME,
-    host: process.env.DATABASE_HOST,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT,
-    dialectOptions: {
-        ssl: true,
-    }
+const {
+  DATABASE_DIALECT = "postgres",
+  DATABASE_HOST = "localhost",
+  DATABASE_PORT = "5432",
+  DATABASE_NAME,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD,
+  DATABASE_SSL = "false",
+} = process.env;
 
+const isLocalhost = DATABASE_HOST === 'localhost' || DATABASE_HOST === '127.0.0.1';
+const needsSSL = DATABASE_SSL === "true" || (!isLocalhost && DATABASE_SSL !== "false");
+
+const sequelize = new Sequelize({
+  dialect: DATABASE_DIALECT,
+  username: DATABASE_USERNAME,
+  host: DATABASE_HOST,
+  password: DATABASE_PASSWORD,
+  database: DATABASE_NAME,
+  port: Number(DATABASE_PORT),
+  dialectOptions: {
+    ssl: needsSSL ? { 
+      rejectUnauthorized: false
+    } : false,
+  },
 });
 
 try {
